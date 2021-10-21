312f0c260d0c2924856d87208fed1b62255d516b
tangwei <tw2066@163.com>
2021-10-21 11:27:08 +0800

    support rpcRoute

--- src/Event/AfterDtoStart.php
+++ src/Event/AfterDtoStart.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Hyperf\DTO\Event;
 
 use Hyperf\HttpServer\Router\RouteCollector;
+use Hyperf\RpcServer\Router\RouteCollector as RpcRouteCollector;
 
 class AfterDtoStart
 {
     public array $serverConfig;
 
-    public RouteCollector $router;
+    /**
+     * @var RouteCollector | RpcRouteCollector
+     */
+    public mixed $router;
 
     public function __construct(array $serverConfig, $router)
     {
