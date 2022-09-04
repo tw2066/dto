6c532b2ea4b5ff85588112d96b106d690bfa9bf8
tangwei <tw2066@163.com>
2022-09-04 20:39:11 +0800

    Optimize the code

---
+++ src/SimpleType.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyperf\DTO;
+
+class SimpleType
+{
+    public const STRING = 'string';
+
+    public const BOOLEAN = 'boolean';
+
+    public const BOOL = 'bool';
+
+    public const INTEGER = 'integer';
+
+    public const INT = 'int';
+
+    public const DOUBLE = 'double';
+
+    public const FLOAT = 'float';
+
+    public const ARRAY = 'array';
+
+    public const OBJECT = 'object';
+}
