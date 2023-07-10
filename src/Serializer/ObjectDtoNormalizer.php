4f4ab41961f74d75c193bf2508d1531e524e7245
tangwei <tw2066@163.com>
2023-07-10 11:26:35 +0800

    phpstan-ignore

--- src/Serializer/ObjectDtoNormalizer.php
+++ src/Serializer/ObjectDtoNormalizer.php
@@ -7,6 +7,7 @@ namespace Hyperf\DTO\Serializer;
 use Hyperf\DTO\Mapper;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
+/* @phpstan-ignore-next-line */
 class ObjectDtoNormalizer extends ObjectNormalizer
 {
     public function denormalize($data, string $type, string $format = null, array $context = [])
