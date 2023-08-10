66fe7267cbf27621ed5dd4da4a889e33ccb2a7bd
tangwei <tw2066@163.com>
2023-08-10 15:20:28 +0800

    验证器支持自定义key,和原生写法
    (cherry picked from commit c5576d8c62cbdd7c0a1f141e89aa76536a3ec321)

--- src/Annotation/Validation/Validation.php
+++ src/Annotation/Validation/Validation.php
@@ -6,15 +6,16 @@ namespace Hyperf\DTO\Annotation\Validation;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_PROPERTY)]
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class Validation extends BaseValidation
 {
     /**
      * Validation constructor.
      */
-    public function __construct(mixed $rule, string $messages = '')
+    public function __construct(mixed $rule, string $messages = '', string $customKey = '')
     {
         $this->rule = $rule;
         $this->messages = $messages;
+        $this->customKey = $customKey;
     }
 }
