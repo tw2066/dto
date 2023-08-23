b10b03b7bda77534f7fafcf3833257f8e167b39b
tangwei <tw2066@163.com>
2023-08-23 09:53:53 +0800

    fix 获取注解

--- src/ApiAnnotation.php
+++ src/ApiAnnotation.php
@@ -17,27 +17,32 @@ class ApiAnnotation
      */
     public static function getProperty($className, $propertyName, $annotationClassName): ?object
     {
+        $className = ltrim($className, '\\');
         $propertyAnnotations = AnnotationCollector::getClassPropertyAnnotation($className, $propertyName);
         return $propertyAnnotations[$annotationClassName] ?? null;
     }
 
     public static function getClassProperty($className, $propertyName): array
     {
+        $className = ltrim($className, '\\');
         return AnnotationCollector::getClassPropertyAnnotation($className, $propertyName) ?? [];
     }
 
     public static function classMetadata($className)
     {
+        $className = ltrim($className, '\\');
         return AnnotationCollector::list()[$className]['_c'] ?? [];
     }
 
     public static function methodMetadata($className)
     {
+        $className = ltrim($className, '\\');
         return AnnotationCollector::list()[$className]['_m'] ?? [];
     }
 
     public static function propertyMetadata($className)
     {
+        $className = ltrim($className, '\\');
         return AnnotationCollector::list()[$className]['_p'] ?? [];
     }
 }
