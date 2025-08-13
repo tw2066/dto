7375c3235a5fe93dd57afa2e2c9699a8c206ca75
tw <tw2066@163.com>
2025-08-13 10:31:13 +0800

    optimize code

--- src/BootApplicationListener.php
+++ src/BootApplicationListener.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace Hyperf\DTO;
 
-use Hyperf\DTO\Annotation\JSONField;
 use Hyperf\DTO\Ast\DtoProxyClass;
 use Hyperf\Event\Annotation\Listener;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\BootApplication;
-use Psr\Container\ContainerInterface;
 
 #[Listener(priority: 999)]
 class BootApplicationListener implements ListenerInterface
 {
     public function __construct(
-        private ContainerInterface $container,
         private DtoProxyClass $dtoProxyClass
     ) {
     }
@@ -30,15 +27,5 @@ class BootApplicationListener implements ListenerInterface
     public function process(object $event): void
     {
         $this->dtoProxyClass->generic();
-        //$this->replaceAliasPropertyAnnotation();
     }
-
-    //public function replaceAliasPropertyAnnotation(): void
-    //{
-    //    $classes = AnnotationCollector::getPropertiesByAnnotation(JSONField::class);
-    //    foreach ($classes as $class) {
-    //        $alias = $class['annotation']->name;
-    //        AnnotationCollector::replaceProperty($class['class'], $alias, $class['property']);
-    //    }
-    //}
 }
