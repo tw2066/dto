1d2d6085ee730888d8e975b2185df7a33bede1e8
tangwei <tw2066@163.com>
2025-09-30 00:53:38 +0800

    验证器增强unique

--- src/Annotation/Validation/Unique.php
+++ src/Annotation/Validation/Unique.php
@@ -5,31 +5,40 @@ declare(strict_types=1);
 namespace Hyperf\DTO\Annotation\Validation;
 
 use Attribute;
+use Hyperf\Context\ApplicationContext;
+use Hyperf\Database\Query\Builder;
+use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\Validation\Rule;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Unique extends BaseValidation
 {
-    protected ?string $ignoreIdColumn = null;
-
     /**
      * 验证字段在给定的数据库表中不得存在.
      */
-    public function __construct(string $table, string $column = 'NULL', ?string $ignoreIdColumn = null, array $wheres = [], public string $messages = '')
+    public function __construct(protected string $table, protected string $column = 'NULL', protected ?string $ignoreIdKey = null, protected ?string $ignoreIdColumn = null, protected array $wheres = [], public string $messages = '')
     {
-        $rule = Rule::unique($table, $column);
-        // $rule->ignore($user->id);
-        //
-        // $rule->where(function ($query) use ($ignoreIdColumn) {
-        //
-        // });
-        if ($wheres) {
-            foreach ($wheres as $column => $where) {
+        parent::__construct($messages);
+    }
+
+    public function getRule(): mixed
+    {
+        $rule = Rule::unique($this->table, $this->column);
+
+        if ($this->ignoreIdKey) {
+            $rule->where(function (Builder $query) {
+                $request = ApplicationContext::getContainer()->get(RequestInterface::class);
+                $excludeId = $request->input($this->ignoreIdKey);
+                $query->where($this->ignoreIdColumn ?: 'id', '<>', $excludeId);
+            });
+        }
+
+        if ($this->wheres) {
+            foreach ($this->wheres as $column => $where) {
                 $rule->where($column, $where);
             }
         }
         $this->rule = $rule;
-        $this->ignoreIdColumn = $ignoreIdColumn;
-        parent::__construct($messages);
+        return $this->rule;
     }
 }
